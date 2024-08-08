3f6cab34ed1cc273c98d502a29b388102d3bc1f6
Indrayani Verulkar <118429007+Indrayani11-15@users.noreply.github.com>
2024-08-08 09:58:44 +0000

    config

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,25 @@
+"use strict"; //treat all JS code as newer version
+
+// alert(3+3)(we are using nodejs not browser)
+//code readability should be high
+
+let name = "Jahan" // string
+let age = 20 //int/number
+let isLoggedIn = false
+
+//numer => 2 to power 53
+//bigint
+//string => " "
+//boolen => true/fase
+//null => standalone value
+//undefined =>
+//symbol =>unique
+//object 
+
+console.log(typeof"Jahan");
+console.log(typeof undefined);//undefined
+console.log(typeof null);//object
+
+
+
+
