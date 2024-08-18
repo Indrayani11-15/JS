7b2b96293eb16ccc02bfde45fed179015dae9224
Indrayani Verulkar <118429007+Indrayani11-15@users.noreply.github.com>
2024-08-18 07:21:08 +0000

    switch break

---
+++ 04_controlflow/switch.js
@@ -0,0 +1,32 @@
+//defualt syntax
+
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = "march"
+switch (month) {
+    case "jan":
+        console.log("Janaury");
+        break;
+    case "feb":
+        console.log("Feb");
+        break;
+    case "march":
+        console.log("March");
+        break;
+    case "april":
+        console.log("April");
+        break;
+
+
+    default:
+        console.log("default case match");
+        
+        break;
+}
